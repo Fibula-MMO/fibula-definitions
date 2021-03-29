aadea98bdc878027a12224e97ddf27464f02f618
Jose Nunez de Caceres <jlnunez89@gmail.com>
2021-03-28 20:44:12 -0700

    add 2 more enumerations, bump pkg version

---
+++ src/Fibula.Definitions/Enumerations/ChatChannelType.cs
@@ -0,0 +1,49 @@
+﻿// -----------------------------------------------------------------
+// <copyright file="ChatChannelType.cs" company="2Dudes">
+// Copyright (c) | Jose L. Nunez de Caceres et al.
+// https://linkedin.com/in/nunezdecaceres
+//
+// All Rights Reserved.
+//
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+// -----------------------------------------------------------------
+
+namespace Fibula.Definitions.Enumerations
+{
+    /// <summary>
+    /// Enumerates the chat channel types.
+    /// </summary>
+    public enum ChatChannelType : ushort
+    {
+        /// <summary>
+        /// The default game channel.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// A channel that appears when rule violations are being reported.
+        /// </summary>
+        RuleViolations,
+
+        /// <summary>
+        /// A channel for trades.
+        /// </summary>
+        Trade,
+
+        /// <summary>
+        /// The real-life channel.
+        /// </summary>
+        RealLife,
+
+        /// <summary>
+        /// The help channel.
+        /// </summary>
+        Help,
+
+        /// <summary>
+        /// A private chat channel.
+        /// </summary>
+        Private,
+    }
+}
---
+++ src/Fibula.Definitions/Enumerations/SpeechType.cs
@@ -0,0 +1,75 @@
+﻿// -----------------------------------------------------------------
+// <copyright file="SpeechType.cs" company="2Dudes">
+// Copyright (c) | Jose L. Nunez de Caceres et al.
+// https://linkedin.com/in/nunezdecaceres
+//
+// All Rights Reserved.
+//
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+// -----------------------------------------------------------------
+
+namespace Fibula.Definitions.Enumerations
+{
+    /// <summary>
+    /// Enumeration of the possible speech types.
+    /// </summary>
+    public enum SpeechType : byte
+    {
+        /// <summary>
+        /// A normal talk.
+        /// </summary>
+        Normal,
+
+        /// <summary>
+        /// Whispering (#w).
+        /// </summary>
+        Whisper,
+
+        /// <summary>
+        /// Yelling (#y).
+        /// </summary>
+        Yell,
+
+        /// <summary>
+        /// Players speaking privately to other players.
+        /// </summary>
+        Private,
+
+        /// <summary>
+        /// Rule violation report.
+        /// </summary>
+        RuleViolationReport,
+
+        /// <summary>
+        /// Rule violation reply.
+        /// </summary>
+        RuleViolationAnswer,
+
+        /// <summary>
+        /// Rule violation continuance.
+        /// </summary>
+        RuleViolationContinue,
+
+        /// <summary>
+        /// Broadcast a message (#b).
+        /// </summary>
+        Broadcast,
+
+        /// <summary>
+        /// Yellow message in chat.
+        /// </summary>
+        ChannelYellow,
+
+        /// <summary>
+        /// Talk in organge.
+        /// </summary>
+        MonsterNormal,
+
+        // ChannelRed = 0x05,   //Talk red on chat - #c
+        // PrivateRed = 0x04,   //Red private - @name@ text
+        // ChannelOrange = 0x05,    //Talk orange on text
+        // ChannelRedAnonymous = 0x05,  //Talk red anonymously on chat - #d
+        // MonsterYell = 0x0E,  //Yell orange
+    }
+}
